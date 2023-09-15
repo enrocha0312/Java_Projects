ccd2325f01d15ca34d84648bc26a7eed930f0826
enrocha0312 <enrocha0312@gmail.com>
2023-09-15 07:04:23 -0300

    api

---
+++ APIs/exemploboaspraticas/exemploboaspraticas/src/main/java/com/eduardondarocha/exemploboaspraticas/ExemploboaspraticasApplication.java
@@ -0,0 +1,13 @@
+package com.eduardondarocha.exemploboaspraticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploboaspraticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploboaspraticasApplication.class, args);
+	}
+
+}
---
+++ APIs/exemploboaspraticas/exemploboaspraticas/src/test/java/com/eduardondarocha/exemploboaspraticas/ExemploboaspraticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardondarocha.exemploboaspraticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploboaspraticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
